16437fad8ad48acc4736cc92b0e0ce4f4a00ca04
Shital Babu Luitel <ctalluitel@gmail.com>
2021-06-06 16:56:25 +0545

    Mongose validation and strialization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect(
     'mongodb://127.0.0.1:27017/task-manager-api',
@@ -14,24 +15,93 @@ const User = mongoose.model(
     'User',
     {
         name: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 7,
+            trim: true,
+            validate(value) {
+                if (value.toLowerCase().includes("password")) {
+                    throw new Error('Password cannot contain "password".')
+                }
+            }
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid.')
+                }
+            }
         },
         age: {
-            type: Number
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error('Age must be a positive number')
+                }
+            }
+        }
+    }
+)
+//
+// const me = new User(
+//     {
+//         name: '  Shital   ',
+//         password: 'helloworld',
+//         email: 'shital@example.com'
+//     }
+// )
+//
+// me.save()
+//     .then(
+//         (result) => {
+//             console.log(me)
+//         }
+//     )
+//     .catch(
+//         (error) => {
+//             console.log(error.errors)
+//         }
+//     )
+//
+
+const Task = mongoose.model(
+    'Task',
+    {
+        description: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
         }
     }
 )
 
-const me = new User({name: 'shital', age: 'nasndk'})
-
-me.save()
-    .then(
-        (result) => {
-            console.log(me)
-        }
-    )
-    .catch(
-        (error) => {
-            console.log('error', error)
-        }
-    )
+//
+// const task = new Task({
+//     description: 'this is task 1.     '
+// })
+//
+// task.save()
+//     .then(
+//         (result) => {
+//             console.log(result)
+//         }
+//     )
+//     .catch(
+//         (error) => {
+//             console.log(error)
+//         }
+//     )
