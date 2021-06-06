7c1604076695d5414ffef7e4ca576a303fcc97b0
Shital Babu Luitel <ctalluitel@gmail.com>
2021-06-06 11:32:59 +0545

    Setting mongoose that interact with mongodb and node js

---
+++ src/db/mongoose.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/task-manager-api',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }
+)
+
+
+const User = mongoose.model(
+    'User',
+    {
+        name: {
+            type: String
+        },
+        age: {
+            type: Number
+        }
+    }
+)
+
+const me = new User({name: 'shital', age: 'nasndk'})
+
+me.save()
+    .then(
+        (result) => {
+            console.log(me)
+        }
+    )
+    .catch(
+        (error) => {
+            console.log('error', error)
+        }
+    )
