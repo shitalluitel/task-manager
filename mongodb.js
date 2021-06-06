125a59af34a7f064a0371f60dbb2f69e2072b8ba
Shital Babu Luitel <ctalluitel@gmail.com>
2021-06-06 10:46:31 +0545

    Update data in mongo db

--- mongodb.js
+++ mongodb.js
@@ -17,96 +17,44 @@ MongoClient.connect(
 
         const db = client.db(databaseName)
 
-        // insert one document into collection
-        // db.collection('users').insertOne(
+        // db.collection('users').updateOne(
         //     {
-        //         name: 'Meera Luitel',
-        //         age: 25
+        //         _id: new ObjectID("60b9202bf3453c528be04c50")
         //     },
-        //     (error, result) => {
-        //         if (error){
-        //             return console.log('Unable to insert user.')
+        //     {
+        //         $set: {
+        //             name: 'Harke'
         //         }
-        //         console.log(result.ops)
+        //     }
+        // ).then(
+        //     (result) => {
+        //         console.log(result)
+        //     }
+        // ).catch(
+        //     (error) => {
+        //         console.log(error)
         //     }
         // )
 
-        // insert many documents into collection at once
-        // db.collection('tasks').insertMany(
-        //     [
-        //         {
-        //             title: 'task 1',
+        // db.collection('tasks').updateMany(
+        //     {
+        //         completed: false
+        //     },
+        //     {
+        //         $set: {
         //             completed: true
-        //         },
-        //         {
-        //             title: 'task 2',
-        //             completed: false
-        //         },
-        //         {
-        //             title: 'task 3',
-        //             completed: false
         //         }
-        //     ],
-        //     {},
-        //     (error, result) => {
-        //         if (error) {
-        //             return console.log('Unable to insert user')
-        //         }
-        //         console.log(result.ops)
+        //     }
+        // ).then(
+        //     (result)=>{
+        //         console.log(result.modifiedCount)
+        //     }
+        // ).catch(
+        //     (error) => {
+        //         console.log(error)
         //     }
         // )
 
-
-        // read document from collection
-
-        // db.collection('users').findOne(
-        //     {
-        //         // name: 'Shital Babu Luitel'
-        //         // age: 25
-        //         _id: ObjectID('60b9202bf3453c528be04c51')
-        //     },
-        //     {},
-        //     (error, user) => {
-        //         if (error){
-        //             return console.log(error)
-        //         }
-        //
-        //         console.log(user)
-        //     }
-        // )
-
-        // db.collection('users').find({age: 25}).toArray(
-        //     (error, users) => {
-        //         console.log(users)
-        //     }
-        // )
-
-        db.collection('tasks').findOne(
-            {
-                _id: new ObjectID('60bc479e09401334beb989ce')
-            },
-            (error, task) => {
-                if (error) {
-                    return console.log(error)
-                }
-
-                console.log(task)
-            }
-        )
-
-        db.collection('tasks').find(
-            {
-                completed: false
-            }
-        ).toArray(
-            (error, tasks) => {
-                if (error) {
-                    return console.log(error)
-                }
-
-                console.log(tasks)
-            }
-        )
 
     }
 )
